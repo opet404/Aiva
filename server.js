55cc77f6646721db47bdb8d3afe34839b2eb9a28
opet404 <ufoxhdyt@gmail.com>
2026-05-02 07:23:46 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app  = express();
 const PORT = 3000;
 
 // 🔑 API KEY
-const GROQ_API_KEY       = "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";
+const GROQ_API_KEY       = "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";
 const OPENROUTER_API_KEY = "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";
 
 // 🧠 SESSION MEMORY
